6f389e9089b91704f25a8d975e894be48e642977
Soggard <e.bernet@laposte.net>
2019-06-05 12:16:42 +0200

    Add bootstrap, add script to display image from input

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 console.log('hello world');
+
+function readURL(input) {
+
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('#baseImage').attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#imgInp").change(function() {
+  readURL(this);
+});
